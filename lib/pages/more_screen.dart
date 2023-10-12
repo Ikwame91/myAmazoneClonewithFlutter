5417e1cdee132f0567bec71a86de1eea6244caf1
Kwame Agyapong <Kwameagyapong91@gmail.com>
2023-10-12 20:01:06 +0000

    3rdd push

--- lib/pages/more_screen.dart
+++ lib/pages/more_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myown_amazone_clone/widgets/cart_screen_appbar.dart';
+import 'package:myown_amazone_clone/widgets/more_screen_dropdown_widget.dart';
 import 'package:myown_amazone_clone/widgets/more_screen_widget.dart';
 
 class MoreScreen extends StatefulWidget {
@@ -10,17 +11,34 @@ class MoreScreen extends StatefulWidget {
 }
 
 class _MoreScreenState extends State<MoreScreen> {
+  _MoreScreenState() {}
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-        appBar: const CartScreenAppbar(),
-        body: Container(
+      appBar: const CartScreenAppbar(),
+      body: SingleChildScrollView(
+        child: Container(
           height: screenSize.height,
           width: screenSize.width,
           color: Colors.white,
-          child: MoreScreenWidget(),
-        ));
+          child: Column(
+            children: [
+              SizedBox(
+                height: screenSize.height / 2,
+                child: const MoreScreenWidget(),
+              ),
+              const Divider(
+                height: 10,
+                thickness: 4,
+                color: Colors.grey,
+              ),
+              MoreScreenDropDown()
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/more_screen_dropdown_widget.dart
@@ -0,0 +1,102 @@
+// import 'package:flutter/material.dart';
+// import 'package:myown_amazone_clone/utils/constants.dart';
+
+// class MoreScreenDropDown extends StatefulWidget {
+//   const MoreScreenDropDown({super.key});
+
+//   @override
+//   State<MoreScreenDropDown> createState() => _MoreScreenDropDownState();
+// }
+
+// class _MoreScreenDropDownState extends State<MoreScreenDropDown> {
+//   String? _selectedVal = '';
+
+//   _MoreScreenDropDownState() {
+//     _selectedVal = shopByCategoryItems[0];
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Column(
+//         children: [
+//           DropdownButton(
+//               value: _selectedVal,
+//               items: shopByCategoryItems
+//                   .map(
+//                     (e) => DropdownMenuItem(
+//                       value: e,
+//                       child: Text(e),
+//                     ),
+//                   )
+//                   .toList(),
+//               onChanged: (val) {
+//                 setState(() {
+//                   _selectedVal = val as String;
+//                 });
+//               }),
+//         ],
+//       ),
+//     );
+//   }
+// }
+import 'package:flutter/material.dart';
+import 'package:myown_amazone_clone/utils/constants.dart';
+
+class MoreScreenDropDown extends StatefulWidget {
+  const MoreScreenDropDown({super.key});
+
+  @override
+  State<MoreScreenDropDown> createState() => _MoreScreenDropDownState();
+}
+
+class _MoreScreenDropDownState extends State<MoreScreenDropDown> {
+  String? _selectedVal = '';
+
+  _MoreScreenDropDownState() {
+    _selectedVal = shopByCategoryItems[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.green,
+                    blurRadius: 10.0,
+                  ),
+                ],
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: _selectedVal,
+                  isExpanded: true,
+                  items: shopByCategoryItems.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedVal = newValue;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
